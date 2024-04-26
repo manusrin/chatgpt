6f3631471fbdbc22f5fb48d1218a12c201f84be8
Manu Srinivasan <manu.srinivasan@gmail.com>
2024-04-26 16:58:00 -0700

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,13 @@
+# This program calls ChatGPT!
+from langchain_openai import ChatOpenAI
+
+# Set variables
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+myQuery = "HOW TO GET KIDS TO PAY ATTENTION?!"
+
+# Call the LLM
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+chatResponse = llm.invoke(myQuery)
+
+# Print output
+print("Heres what ChatGPT said \n\n", chatResponse.content)
